84ff3800ea285ca970351178f745feb9524d26ae
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-10-24 23:42:30 +0530

    handle env

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j_@@@vs6pp@i1s+e@f42r3hap4l(e-6u)%-0@*0_imxo7@4i2s'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DB_ENGINE')
 
 ALLOWED_HOSTS = []
 
@@ -75,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-from decouple import config
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
