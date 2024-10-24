4453d688cf1fed82882d359f899c181cdca92183
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-10-25 00:24:16 +0530

    add admin

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,8 @@
 # users/urls.py
-from django.contrib import admin
 from django.urls import path
 from .views import submit_user_details, calculate_quote, create_quote
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('submit-details/', submit_user_details, name='submit_details'),
     path('calculate-quote/', calculate_quote, name='calculate_quote'),
     path('create-quote/', create_quote, name='create_quote'),
