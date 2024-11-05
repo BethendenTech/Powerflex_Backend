95afae9d3ff1139fdca3fc26d0c7703eb60d68fd
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-11-05 22:47:28 +0530

    add product module

---
+++ product/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="products")
+    name = models.CharField(max_length=100)
+    unique_code = models.TextField(blank=True, null=True)
+    model_name = models.TextField(blank=True, null=True)
+    model_year = models.TextField(blank=True, null=True)
+    country_of_origin = models.TextField(blank=True, null=True)
+    capacity_w = models.TextField(blank=True, null=True)
+    solar_panel_efficiency = models.TextField(blank=True, null=True)
+    price_usd = models.TextField(blank=True, null=True)
+    voc_v = models.TextField(blank=True, null=True)
+    isc_a = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
