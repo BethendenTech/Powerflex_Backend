0b561b22e2c92a2067047fc6a4c3bda12a9fcd5e
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-11-15 16:39:19 +0530

    add STATIC_URL

--- backend/settings.py
+++ backend/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
     "wkwog8cgsoo8004ssssk4kwg.142.93.253.117.sslip.io",
     "142.93.253.117",
     "localhost",
+    "127.0.0.1",
 ]
 
 
@@ -143,8 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
