a487521b518102b52e56780557dbe0b91c46b8ed
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-11-19 16:41:45 +0530

    add installer_commission

--- setting/admin.py
+++ setting/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
 from .models import Settings
 
+
 @admin.register(Settings)
 class SettingsAdmin(admin.ModelAdmin):
-    list_display = ['vat', 'profit_margin', 'installation_margin']
+    list_display = [
+        "vat",
+        "profit_margin",
+        "installation_margin",
+        "installer_commission",
+    ]
 
     def has_add_permission(self, request):
         # Prevent adding more than one instance
