835557a17d20f9e628ca32c135a103073d3bd827
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-11-23 18:28:41 +0530

    ff

--- backend/urls.py
+++ backend/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("website.urls")),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
 ]
