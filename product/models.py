4f44b592ec02da443f39ec234908d528894bc953
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-12-02 20:20:42 +0530

    adsd

--- product/models.py
+++ product/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
-    
+
+
 class Brand(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
@@ -14,8 +16,11 @@ class Brand(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products"
+    )
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="products")
     name = models.CharField(max_length=100)
     unique_code = models.TextField(blank=True, null=True)
