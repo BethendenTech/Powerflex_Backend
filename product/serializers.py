b115407c5b24d9850498004bc3b801b0227f89d8
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-12-05 00:47:02 +0530

    add request

---
+++ product/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Appliance, ApplianceCategory
+
+
+class ApplianceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appliance
+        fields = ["id", "name", "power_w", "description"]
+
+
+class ApplianceCategorySerializer(serializers.ModelSerializer):
+    appliances = ApplianceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ApplianceCategory
+        fields = ["id", "name", "appliances"]
