53dea507dcb9db8f9cd1504fa77d552c34f0858f
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2025-02-08 18:29:17 +0530

    add appliances

--- package/serializers.py
+++ package/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from .models import Package
+from .models import Package, Appliance
+
+
+class ApplianceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appliance
+        fields = [
+            "id",
+            "name",
+            "power_w",
+            "description",
+        ]  # Add fields you want to include
 
 
 class PackageSerializer(serializers.ModelSerializer):
+    appliances = ApplianceSerializer(many=True, read_only=True)  # Nested serializer
+
     class Meta:
         model = Package
         fields = [
@@ -13,5 +26,5 @@ class PackageSerializer(serializers.ModelSerializer):
             "discount_price",
             "runtime",
             "description",
-            "appliances",
+            "appliances",  # Include the nested serializer
         ]
