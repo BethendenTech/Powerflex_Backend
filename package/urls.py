c3c63d1dd7108c7acdf0dc13718ca08403f0725a
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2025-02-14 12:25:36 +0530

    add package api

--- package/urls.py
+++ package/urls.py
@@ -1,8 +1,9 @@
 # users/urls.py
 from django.urls import path
-from .views import package_list, package_detail
+from .views import package_list, package_detail, package_request
 
 urlpatterns = [
     path("packages/", package_list, name="package-list"),
     path("package-detail/<int:pk>/", package_detail, name="package-detail"),
+    path("package-request/", package_request, name="package-request"),
 ]
--- package/views.py
+++ package/views.py
@@ -1,7 +1,9 @@
+from datetime import date
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Package
 from .serializers import PackageSerializer
+
 
 # Create your views here.
 @api_view(["GET"])
@@ -16,3 +18,22 @@ def package_detail(request, pk):
     package = Package.objects.prefetch_related("appliances").get(pk=pk)
     serializer = PackageSerializer(package)
     return Response(serializer.data)
+
+
+@api_view(["POST"])
+def package_request(request):
+    package_id = request.data.get("package_id")
+
+    package = Package.objects.get(pk=package_id)
+
+    total_capacity = 0
+
+    for appliance in package.appliances.all():
+        total_capacity += appliance.power_w
+
+    data = {
+        "total_capacity": total_capacity,
+        "total_cost": package.price,
+    }
+
+    return Response(data)
